2ffbb58aae11a23a9938d8af338c9a042c6734ff
KangHyun Lee <rkd7327@naver.com>
2018-02-06 12:23:14 +0900

    client메퍼,dao,dto,test완료

---
+++ src/main/java/Project_JSP/dto/Client.java
@@ -0,0 +1,125 @@
+package Project_JSP.dto;
+
+import java.util.Date;
+
+public class Client {
+	private int clientNum;
+	private String id;
+	private String pw;
+	private String nameKo;
+	private String nameEn;
+	private Date birth;
+	private String email;
+	private String phone;
+	private String home;
+	private String address;
+	private ClientGrade clientGrade;
+
+	public int getClientNum() {
+		return clientNum;
+	}
+
+	public void setClientNum(int clientNum) {
+		this.clientNum = clientNum;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getNameKo() {
+		return nameKo;
+	}
+
+	public void setNameKo(String nameKo) {
+		this.nameKo = nameKo;
+	}
+
+	public String getNameEn() {
+		return nameEn;
+	}
+
+	public void setNameEn(String nameEn) {
+		this.nameEn = nameEn;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getHome() {
+		return home;
+	}
+
+	public void setHome(String home) {
+		this.home = home;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public ClientGrade getClientGrade() {
+		return clientGrade;
+	}
+
+	public void setClientGrade(ClientGrade clientGrade) {
+		this.clientGrade = clientGrade;
+	}
+
+	public Client(String id, String pw, String nameKo, String nameEn, Date birth, String email, String phone,
+			String home, String address, ClientGrade clientGrade) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		this.nameKo = nameKo;
+		this.nameEn = nameEn;
+		this.birth = birth;
+		this.email = email;
+		this.phone = phone;
+		this.home = home;
+		this.address = address;
+		this.clientGrade = clientGrade;
+	}
+
+	public Client() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/resources/Project_JSP/service/test/TestClientDaoService.java
@@ -0,0 +1,94 @@
+package Project_JSP.service.test;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import Project_JSP.dao.ClientDao;
+import Project_JSP.dto.Client;
+import Project_JSP.mvc.util.MySqlSessionFactory;
+
+public class TestClientDaoService {
+/*
+	@Test
+	public void selectacilityType() {
+		try (SqlSession session = MySqlSessionFactory.openSession()) {
+			ClientDao dao = session.getMapper(ClientDao.class);
+			List<Client> list = dao.selectClient();
+
+			for (Client f : list) {
+				System.out.println(f.getNameKo());
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}*/
+
+/*	@Test
+	public void selectClientNum() {
+		try (SqlSession session = MySqlSessionFactory.openSession()) {
+			ClientDao dao = session.getMapper(ClientDao.class);
+			Client client = new Client();
+			client.setClientNum(1);
+			Client selectClient = dao.selectClientNum(client);
+			System.out.println(selectClient.getNameKo());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}*/
+	
+/*	@Test
+	public void insertClient() {
+		try (SqlSession session = MySqlSessionFactory.openSession()) {
+			ClientDao dao = session.getMapper(ClientDao.class);
+			
+			Client client = new Client();
+			client.setClientNum(12);
+			client.setNameEn("비즈니스센터");
+			
+			
+			dao.insertClient(client);
+			session.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}*/
+	
+/*	@Test
+	public void updateClient() {
+		try (SqlSession session = MySqlSessionFactory.openSession()) {
+			ClientDao dao = session.getMapper(ClientDao.class);
+			
+			Client client = new Client();
+			client.setClientNum(1);
+			client.setHome("SDf");
+			client.setId("Asd");
+			dao.updateClient(client);
+			session.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}*/
+
+/*	@Test
+	public void deleteClient() {
+		try (SqlSession session = MySqlSessionFactory.openSession()) {
+			ClientDao dao = session.getMapper(ClientDao.class);
+			
+			Client client = new Client();
+			client.setClientNum(1);
+			
+			dao.deleteClient(client);
+			session.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}*/
+}
