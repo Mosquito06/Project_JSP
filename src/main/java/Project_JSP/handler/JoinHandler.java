41592f49e73fb28cb3b5c13880e0adbdc278d69d
DaHee Choi <ekgml9904@naver.com>
2018-02-07 17:45:50 +0900

    회원가입

---
+++ src/main/java/Project_JSP/handler/JoinHandler.java
@@ -0,0 +1,20 @@
+package Project_JSP.handler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Project_JSP.mvc.controller.CommandHandler;
+
+public class JoinHandler implements CommandHandler {
+	private static final String JOIN_VIEW = "/WEB-INF/view/join/joinForm.jsp";
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		if(req.getMethod().equalsIgnoreCase("get")){
+			return JOIN_VIEW;
+		}else if(req.getMethod().equalsIgnoreCase("post")){
+			
+		}
+		return null;
+	}
+
+}
