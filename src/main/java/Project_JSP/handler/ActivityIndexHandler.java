59645da7ec3b31fec7e491035da7b40c0f8c9469
S_YoonPyo <ypyo09@naver.com>
2018-02-07 11:11:54 +0900

    경로 문제 수정

---
+++ src/main/java/Project_JSP/handler/ActivityIndexHandler.java
@@ -0,0 +1,15 @@
+package Project_JSP.handler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Project_JSP.mvc.controller.CommandHandler;
+
+public class ActivityIndexHandler implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		return "/WEB-INF/view/activity/activity.jsp";
+	}
+
+}
